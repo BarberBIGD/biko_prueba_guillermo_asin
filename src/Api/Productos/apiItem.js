0fc7e537fe545f8d3c3120b21cd8b1f0e4d48ee8
Guillermo Asín Senosiain <bubblesbubblesdev@gmail.com>
2024-01-13 13:01:45 +0100

    Primer Test Pasado añadiendo control de errores a apiItem.js

--- src/Api/Productos/apiItem.js
+++ src/Api/Productos/apiItem.js
@@ -4,7 +4,16 @@ const API_URL = "https://dulces-petalos.herokuapp.com/api/product";
 
 
 const obtenerProductos = async () =>  {
-    
+    try {
+        const respuesta = await fetch(API_URL);
+        if (!respuesta.ok){
+            throw new Error("No se han podido obtener los productos de la API");
+        }
+        
+    } catch (error) {
+        console.log("Error", error);
+        return [];
+    }
 
 }
 
